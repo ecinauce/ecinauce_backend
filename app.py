d4fce00f24c63399ab67408d2c11fddd9fbde7af
ecinauce <nicololynsuecuacion@gmail.com>
2021-08-06 21:55:06 +0800

    [UPDATE] Added CORS compatibility with JS access to vue. Modified POST resource access from forms to json data from vue.

--- app.py
+++ app.py
@@ -1,36 +1,44 @@
+import jwt, os
+from traceback import print_exc
 from flask import Flask, session, request, render_template, url_for, redirect
+from flask_cors import CORS
 from flask.json import jsonify
 from passlib.hash import sha256_crypt
 from .model.model_user import User
 from .settings import project_db
-from .utils import validate_user, validate_registration
+from .utils import validate_user, validate_registration, bson_parse
+from functools import wraps
 
 
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = os.urandom(16)
+CORS(app)
 
+# CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
 	if request.method == 'POST':
-		if validate_registration(request.form['username'], request.form["password"], request.form["vrf_password"]):
-			user_item = project_db["users"].insert_one({"username": request.form['username']})
+		output = dict(request.get_json())
+		if validate_registration(output["username"], output["password"], output["vrf_password"]):
+			user_item = project_db["users"].insert_one({"username": output['username']})
 			user_id = user_item.inserted_id
 
-			password = request.form['password']
+			password = output['password']
 			project_db["accounts"].insert_one({"user_id": user_id, "password": sha256_crypt.encrypt(password)})
 			return jsonify(User(user_id).get_json())
 		else:
-			return '''<b>Registration Error, user might already exist or there is password mismatch.</b>'''
+			return {"message":"Registration Error, user might already exist or there is password mismatch."}
 	return render_template('register.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
-		if validate_user(request.form["username"], request.form["password"]):
-			session['username'] = request.form['username']
-			return redirect(url_for('index'))
+		output = dict(request.get_json())
+		if validate_user(output["username"], output["password"]):
+			session['username'] = output['username']
+			return {"message": "Logged in"}
 		else:
 			return '''
 				<b>Login</b>
@@ -64,8 +72,16 @@ def logout():
 def index():
 	if 'username' in session:
 		user = project_db["users"].find_one({"username": session["username"]})["_id"]
-		return User(user).get_json()
-	return 'You are not logged in'
+		output = {
+			"status": "ok",
+			"payload": User(user).get_json(),
+			"session": session
+		}
+		return output
+	# return session
+	return {
+		"status": "Not logged in"
+	}
 
 
 @app.route("/user/<username>")
@@ -75,8 +91,12 @@ def get_user(username):
 			user = project_db["users"].find_one({"username": username})["_id"]
 			return User(user).get_json()
 		else:
-			return 'Invalid User'
-	return 'You are not logged in'
+			return {
+				"status": 'Invalid User'
+			}
+	return {
+		"status": "Not logged in"
+		}
 	
 
 @app.route("/update_user", methods=['POST'])
@@ -85,21 +105,35 @@ def update():
 		if request.method == "POST":
 			user_id = project_db["users"].find_one({"username": session['username']})["_id"]
 			user = User(user_id).get_json()
-			form = request.form.to_dict()
+			form = output.to_dict()
 			user.update(form)
 			return user
 		return redirect(url_for('index'))
 	return redirect(url_for('index'))
 
 
-@app.route("/update_experience", methods=['POST'])
-def update():
-	if 'username' in session:
-		if request.method == "POST":
-			user_id = project_db["users"].find_one({"username": session['username']})["_id"]
-			user = User(user_id).get_json()
-			form = request.form.to_dict()
-			user.update(form)
-			return user
-		return redirect(url_for('index'))
-	return redirect(url_for('index'))
+# @app.route("/update_experience", methods=['POST'])
+# def update():
+# 	if 'username' in session:
+# 		if request.method == "POST":
+# 			user_id = project_db["users"].find_one({"username": session['username']})["_id"]
+# 			user = User(user_id).get_json()
+# 			form = output.to_dict()
+# 			user.update(form)
+# 			return user
+# 		return redirect(url_for('index'))
+# 	return redirect(url_for('index'))
+
+
+@app.route("/render_html")
+def rendering():
+	return render_template('form_user.html')
+
+
+@app.route("/users")
+def get_users():
+	users = [bson_parse(i) for i in project_db["users"].find()]
+	return {
+		"status": "ok",
+		"payload": users
+	}
---
+++ model/model_project.py
@@ -0,0 +1,6 @@
+class Project:
+	def __init__(self, _id):
+		self._id = _id
+		self.name = ""
+		self.repo = ""
+		self.specs = {}
